7eddf6c1feecbda80cbab3567371946f56ceb9df
Migvalentini <111707132+Migvalentini@users.noreply.github.com>
2023-05-15 15:06:36 -0300

    Create haxa.ts

---
+++ js-ts/haxa.ts
@@ -0,0 +1,13 @@
+function converter(num: number) {
+
+   let resto:number = 0
+   let haxadecimal: string = ''
+   let stringresto: string = ''
+
+   while (num < 16) {
+      resto = num % 16
+      num = num / 16
+      if ()
+   }
+  
+}
